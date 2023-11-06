f709db508f75decee23eed22c31aecd1ee67fc6d
ScarlettDun <148828774+ScarlettDun@users.noreply.github.com>
2023-11-06 23:37:56 +0000

    Deploying to gh-pages from @ ScarlettDun/ScarlettDun.github.io@1df63ad1119833281054f5cb73d35d6ee8c37906 🚀

--- assets/pageview-7e465b09.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./app-77d9a0de.js";const a=()=>e({serverURL:"https://waline-comment.vuejs.press"});export{a as updatePageview};
---
+++ assets/pageview-c77f2e29.js
@@ -0,0 +1,1 @@
+import{P as e}from"./app-6f651342.js";const t=()=>e({serverURL:"https://comment.scarlettdun.com/"});export{t as updatePageview};
