35a0d0034760f2eae1d41ae47a3100bbfdc2905f
ScarlettDun <Scarlett_Yao2002@163.com>
2023-11-16 07:55:11 +0800

    xsxxs

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -13,11 +13,11 @@ export default navbar([
   icon:"exercise",
   link:"#"
 },
-{
-  text:"分享经历"
-  icon:"blog",
-  link:"#"
-},
+// {
+//   text:"分享经历"
+//   icon:"blog",
+//   link:"#"
+// },
 {
   text:"日常琐碎",
   icon:"read",
