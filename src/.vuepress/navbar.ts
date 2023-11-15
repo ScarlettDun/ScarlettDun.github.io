cce5473100ae18aefbf017d6998312e1952ebcbf
AndersonHJB <cleland1432803776@icloud.com>
2023-11-16 07:39:48 +0800

    提交

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,25 +2,25 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-{
-  text:"详细规划",
-  icon:"list",
-  link:"#"
-},
-{
-  text:"学习过程",
-  icon:"exercise",
-  link:"#"
-},
-{
-  text:"分享经历"
-  icon:"blog",
-  link:"#"
-},
-{
-  text:"日常琐碎",
-  icon:"read",
-  link:"#"
-}
-  
+  {
+    text: "详细规划",
+    icon: "list",
+    link: "#"
+  },
+  {
+    text: "学习过程",
+    icon: "exercise",
+    link: "#"
+  },
+  {
+    text: "分享经历",
+    icon: "blog",
+    link: "#"
+  },
+  {
+    text: "日常琐碎",
+    icon: "read",
+    link: "#"
+  }
+
 ]);
