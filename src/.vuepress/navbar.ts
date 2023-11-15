b4880d927547f1d5fa2cd4b01bfe155105bf64a3
ScarlettDun <Scarlett_Yao2002@163.com>
2023-11-16 07:36:21 +0800

    修好bug，升级了新版本

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -6,6 +6,21 @@ export default navbar([
   text:"详细规划",
   icon:"list",
   link:"#"
+},
+{
+  text:"学习过程",
+  icon:"exercise",
+  link:"#"
+},
+{
+  text:"分享经历"
+  icon:"blog",
+  link:"#"
+},
+{
+  text:"日常琐碎",
+  icon:"read",
+  link:"#"
 }
   
 ]);
