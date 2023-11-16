0ad53522bb6cd685f3d7ee6923a5b74477d1c925
ScarlettDun <Scarlett_Yao2002@163.com>
2023-11-16 08:06:57 +0800

    修改了分栏

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -3,25 +3,43 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-{
-  text:"详细规划",
-  icon:"list",
-  link:"#"
-},
-{
-  text:"学习过程",
-  icon:"exercise",
-  link:"#"
-},
-// {
-//   text:"分享经历"
-//   icon:"blog",
-//   link:"#"
-// },
-{
-  text:"日常琐碎",
-  icon:"read",
-  link:"#"
-}
-  
+  {
+    text: "详细规划",
+    icon: "list",
+    children: [
+      {text:"年",icon:"",link:"#"},
+      {text:"月",icon:"",link:"#"},
+      {text:"日",icon:"",link:"#"},
+    ]
+  },
+  {
+    text: "学习过程",
+    icon: "exercise",
+    children: [
+      {text:"Python",icon:"python",link:"#"},
+      {text:"Website",icon:"",link:"#"},
+      {text:"Some skill",icon:"",link:"#"},
+    ]
+  },
+  {
+    text:"分享经历",
+    icon:"blog",
+    children: [
+      {text:"学校",icon:"",link:"#"},
+      {text:"工作",icon:"",link:"#"},
+      {text:"日常",icon:"",link:"#"},
+      {text:"旅游",icon:"",link:"#"},
+    ]
+  },
+  {
+    text: "日常琐碎",
+    icon: "read",
+    children: [
+      {text:"事物看法",icon:"",link:"#"},
+      {text:"我的思考",icon:"",link:"#"},
+      {text:"一些清单",icon:"",link:"#"},
+      {text:"阅读笔记",icon:"",link:"#"},
+    ]
+  }
+
 ]);
