26497dcfb6d8f20c12a5f03c48b35d690309ab81
ScarlettDun <148828774+ScarlettDun@users.noreply.github.com>
2023-11-29 15:50:51 +0000

    Deploying to gh-pages from @ ScarlettDun/ScarlettDun.github.io@3a451cb2fe5f0a251078cbf4d7bb215b976b35c9 🚀

--- assets/pageview-2ac81e4b.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./app-b5e89026.js";const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-b5e89026.js").then(r=>r.Z),[]);return e({serverURL:"https://comment.scarlettdun.com/"})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
