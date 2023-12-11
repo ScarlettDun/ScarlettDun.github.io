0d67089f6045baeb9884dd99172441524d9ee643
ScarlettDun <148828774+ScarlettDun@users.noreply.github.com>
2023-12-11 01:57:41 +0000

    Deploying to gh-pages from @ ScarlettDun/ScarlettDun.github.io@4a5666b68a5f792cbbc2e353286683e99563e98f 🚀

--- assets/pageview-_z1uZ-_l.js
+++
@@ -1,7 +0,0 @@
-import{a6 as t}from"./app-DSxQcl1d.js";var n={provider:"Waline",dark:"auto",serverURL:"https://comment.scarlettdun.com/",reaction:["/Waline/emoji/wow.png","/Waline/tieba/tieba_sunglasses.png","/Waline/tieba/tieba_pick_nose.png","/Waline/tieba/1f613.png","/Waline/emoji/sleepy.png","/Waline/emoji/lemon.png"],comment:!0,pageview:!0,copyright:!1};const r=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-DSxQcl1d.js").then(a=>a.af),__vite__mapDeps([]));return e({serverURL:n.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{r as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
